418f5de5f30f59199a18c8c41b67d5bad9c8e7a2
Jiho Park <sei012003@gmail.com>
2025-07-09 05:52:41 +0000

    BA1J

--- BA1J.py
+++ BA1J.py
@@ -1,4 +1,4 @@
-# Find the Most Frequent Words with Mismathces in a String
+# Find Frequent Words with Mismatches and Reverse Complements
 
 def Hamming_distance(str1, str2):
     return sum(1 for i,j in zip(str1, str2) if i != j)
@@ -17,12 +17,21 @@ def d_neighborhood(string, d):
             subset.add(string[0] + kmer)
     return subset
 
-def freq_words_wM(text, k, d):
+def reverse_strand(pattern):
+    bases = {"A":"T","G":"C","C":"G","T":"A"}
+    return "".join(bases[i] for i in reversed(pattern))
+
+# main
+def freq_words_wM_reverse(text, k, d):
     count = {}
-    for i in range(len(text)-k+1):
+    n = len(text)
+    for i in range(n-k+1):
         kmer = text[i:i+k]
         for nei in d_neighborhood(kmer,d):
             count[nei] = count.get(nei,0) +1
+        for rev_nei in d_neighborhood(reverse_strand(kmer),d):
+            count[rev_nei] = count.get(rev_nei,0) +1
+        
     return [k for k,v in count.items() if v == max(count.values())]
 
 # Input 
@@ -31,5 +40,5 @@ k = 4
 d = 1
 
 # Output
-result = freq_words_wM(string,k,d)
+result = freq_words_wM_reverse(string,k,d)
 print(" ".join(result))
--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Problems in Interest
 
 BA1I.py : HARD
 (Find the Most Frequent Words with Mismatches in a String)
+BA1J.py : HARD
+(Find Frequent Words with Mismatches and Reverse Complements)
 
 BA1N.py : recursion practice
 (Generate the d-Neighborhood of a String)
