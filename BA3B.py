2b9f3219096d73bee361f4f73684032d27e85b41
Macaberry <sei012003@gmail.com>
2025-09-18 17:36:06 +0900

    BA3B

--- BA3B.py
+++ BA3B.py
@@ -1,11 +1,31 @@
 # String Spelled by a Genome Path Problem
 
-def genomePath(patterns):
-    return patterns[0] + "".join(pattern[-1] for pattern in patterns[1:])
+def main():
+    # Input
+    inputs = get_Input(r"C:\Users\user\Desktop\Rosalind_textbook_public\rosalind_ba3b.txt")
+    sKmers = inputs
 
-# Input
-with open("rosalind_ba3b.txt","r") as f:
-    inputs = f.read().splitlines()
-# Output
-result = genomePath(inputs)
-print(result)
+    result = Reconstruct_Str_from_kmers(sKmers)
+    
+    # Output
+    write_Output(result)
+    return 0
+
+def Reconstruct_Str_from_kmers(sKmers):
+    sGenome = sKmers[0]
+    for sKmer in sKmers[1:]:
+        sGenome += sKmer[-1]
+    return sGenome
+
+def get_Input(filename):
+    with open(filename, "r") as f:
+        inputs = f.read().split()
+    return inputs
+
+def write_Output(result):
+    with open("rosalind_ba3b_result.txt","w") as f:
+        f.write(result)
+
+
+if __name__ == "__main__":
+    main()
