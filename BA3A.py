c3e5ad11b774c4edfa30fea366b99a4d45d733d1
Macaberry <sei012003@gmail.com>
2025-09-18 16:08:53 +0900

    BA3A

--- BA3A.py
+++ BA3A.py
@@ -2,15 +2,26 @@
 
 def main():
     # Input
-    k = 5
-    sString = "CAATCCAAC"
+    inputs = get_Input(r"C:\Users\user\Desktop\Rosalind_textbook_public\rosalind_ba3a.txt")
+    k = int(inputs[0])
+    sString = inputs[1]
 
-    # Output
     result = Composition(k, sString)
-    print("\n".join(result))
+    write_output(result)
+    return 0
 
 def Composition(k, text):
     return list(sorted([text[i:i+k] for i in range(len(text)-k+1)]))
+
+def get_Input(filename):
+    with open(filename, "r") as f:
+        inputs = f.read().split()
+    return inputs
+
+def write_output(result):
+    with open("rosalind_ba3a_result.txt","w") as f:
+        f.write("\n".join(result))
+
 
 if __name__ == "__main__":
     main()
