203e9552415e1d3b92af69326c2ad6e16b68bb15
Macaberry <sei012003@gmail.com>
2025-09-18 15:49:18 +0900

    BA3A

--- BA3A.py
+++ BA3A.py
@@ -1,10 +1,16 @@
 # Generate the k-mer Composition of a String
 
+def main():
+    # Input
+    k = 5
+    sString = "CAATCCAAC"
+
+    # Output
+    result = Composition(k, sString)
+    print("\n".join(result))
+
 def Composition(k, text):
     return list(sorted([text[i:i+k] for i in range(len(text)-k+1)]))
-# Input
-k = 5
-string = "CAATCCAAC"
-# Output
-print("\n".join(Composition(k,string)))
 
+if __name__ == "__main__":
+    main()
