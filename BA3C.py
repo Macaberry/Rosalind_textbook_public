79d8207ec4c34b3899b6f6e0ef24e5b2e9a2a4e9
Macaberry <sei012003@gmail.com>
2025-09-30 16:18:15 +0900

    BA3C

--- BA3C.py
+++ BA3C.py
@@ -1,23 +1,37 @@
 # Construct the Overlap Graph of a Collection of k-mers
+from collections import defaultdict
+import random
 
-def Overlap(patterns):
-    # (1) store prefix first for O(n)
-    pre_dct = {}
-    for pattern in patterns:
-        pre_dct.setdefault(pattern[:-1],[]).append(pattern)
-    # (2) find each suffix in pre_dct
-    overlaplist = []
-    for p_suf in patterns:
-        if p_suf[1:] in pre_dct:
-            for p_pre in  pre_dct[p_suf[1:]]:
-                if p_suf != p_pre:                 
-                    overlaplist.append((p_suf, p_pre))
-    return sorted(overlaplist)
+def main():
+    # Input
+    inputs = get_Input(r"C:\Users\user\Desktop\Rosalind_textbook_public\rosalind_ba3c.txt")
+    
+    sOverlapGraphDic = Make_a_OverlapGraph(inputs)
 
-# Input
-with open("rosalind_ba3c.txt","r") as f:
-    inputs = f.read().splitlines()
-# Output
-result = Overlap(inputs)
-for i in range(len(result)):
-    print(result[i][0]+" -> "+result[i][1])
+    # Output
+    write_Output(sOverlapGraphDic)
+    return 0
+
+def Make_a_OverlapGraph(sKmers):
+    sGraphDic = {}
+    for sPreKmer in sKmers:
+        sGraphDic[sPreKmer] = []
+        for sNextKmer in sKmers:
+            if sPreKmer[1:] == sNextKmer[:-1] and sPreKmer != sNextKmer:
+                sGraphDic[sPreKmer].append(sNextKmer)
+    return sGraphDic
+
+def get_Input(filename):
+    with open(filename, "r") as f:
+        inputs = f.read().split()
+    return inputs
+
+def write_Output(sOverlapGraphDic):
+    sOverlapGraphDic = dict(sorted(sOverlapGraphDic.items()))
+    with open("rosalind_ba3c_result.txt","w") as f:
+        for k,valueList in sOverlapGraphDic.items():
+            if len(valueList):
+                f.write(f'{k} -> {valueList[0]}\n') 
+
+if __name__ == "__main__":
+    main()
