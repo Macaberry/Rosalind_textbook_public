60946ff67e6a6f6ac1f7b301cf2f059f741011ba
Macaberry <sei012003@gmail.com>
2025-10-04 13:42:09 +0900

    BA3E

--- BA3E.py
+++ BA3E.py
@@ -1,16 +1,45 @@
 # Construct the De Bruijn Graph of a Collection of k-mers
 
-def CompositionGraph(patterns):
-    dct = {}
-    for p in patterns:
-        dct.setdefault(p[:-1],[]).append(p[1:])
-    return dict(sorted(dct.items()))
+def main():
+    
+    InputFilename = "rosalind_ba3e.txt"
+    sKmerlist = get_Input(InputFilename)
 
-# Input
-with open("rosalind_ba3e.txt","r") as f:
-    inputs = f.read().splitlines()
+    sDeBruijnGraph = construct_DeBruijnGraph(sKmerlist)
 
-# Output
-result = CompositionGraph(inputs)
-for key, value in result.items():
-    print(key+" -> "+",".join(sorted(value)))
+    OutputFilename = "rosalind_ba3e_result.txt"
+    write_Output(OutputFilename, sDeBruijnGraph)
+
+    return 0
+    
+def construct_DeBruijnGraph(sKmerlist):
+    sDeBruijndct = {}
+
+    for sKmer in sKmerlist:
+        prefix_sKmer = sKmer[:-1]
+        suffix_sKmer = sKmer[1:]
+
+        sDeBruijndct.setdefault(prefix_sKmer,[])
+
+        sDeBruijndct[prefix_sKmer].append(suffix_sKmer)
+    
+    return sDeBruijndct        
+
+def get_Input(filename):
+
+    with open(filename, "r") as f:
+        inputs = f.read().split()
+
+    return inputs
+
+def write_Output(filename, sDebruinDct):
+    
+    with open(filename,"w") as f:
+        for k,v in sDebruinDct.items():
+            v = sorted(v)
+            f.write(f"{k} -> {",".join(v)}\n")
+    
+    return 0
+
+if __name__ == "__main__":
+    main()
